b8072ff7cddd7aafac0711f60916e4838ec7250d
Nikolay <niko.dr@gmail.com>
2021-01-16 23:37:33 +0200

    Initial Commit

---
+++ COVID_Mac_OS/COVID_Mac_OSApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_Mac_OSApp.swift
+//  COVID_Mac_OS
+//
+//  Created by Nikolay Nikolayenko on 16/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct COVID_Mac_OSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COVID_Mac_OS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVID_Mac_OS
+//
+//  Created by Nikolay Nikolayenko on 16/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
